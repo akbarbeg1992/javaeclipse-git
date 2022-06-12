0efc80284c1df1a8df65d007c70afad361859523
Mirza Akbar Beg <akbarbeg1992@gmail.com>
2022-06-13 06:36:36 +1000

    Added functionality to receive/send JSON

--- test/src/main/java/com/seismatest/test/TestRestController.java
+++ test/src/main/java/com/seismatest/test/TestRestController.java
@@ -1,5 +1,26 @@
 package com.seismatest.test;
 
-public class TestRestController {
+import java.util.ArrayList;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRestController {
+	
+	@RequestMapping (
+		value = "/runApp",
+		method = RequestMethod.POST,
+		produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }
+	)
+	
+	@ResponseBody
+	public ArrayList<PaySlip> runApplication( @RequestBody ArrayList<Employee> employees ) {
+		TestView testView = new TestView( employees );
+		return testView.getPaySlips();
+	}
 }
