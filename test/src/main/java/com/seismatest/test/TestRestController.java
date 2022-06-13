342ecabc93b7600e6d40b3379f09f7fe508810d3
Mirza Akbar Beg <akbarbeg1992@gmail.com>
2022-06-14 07:56:50 +1000

    Cleaned up code

--- test/src/main/java/com/seismatest/test/TestRestController.java
+++ test/src/main/java/com/seismatest/test/TestRestController.java
@@ -3,22 +3,18 @@ package com.seismatest.test;
 import java.util.ArrayList;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestRestController {
 	
-	@RequestMapping (
+	@PostMapping (
 		value = "/runApp",
-		method = RequestMethod.POST,
 		produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }
 	)
 	
-	@ResponseBody
 	public ArrayList<PaySlip> runApplication( @RequestBody ArrayList<Employee> employees ) {
 		TestView testView = new TestView( employees );
 		return testView.getPaySlips();
