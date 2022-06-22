0c03fb999158a219589e816acfcf2999f673da12
Mirza Akbar Beg <akbarbeg1992@gmail.com>
2022-06-22 15:20:31 +1000

    Changed Logic

--- test/src/main/java/com/seismatest/test/TestController.java
+++ test/src/main/java/com/seismatest/test/TestController.java
@@ -17,11 +17,14 @@ public class TestController {
 	
 	private ArrayList<Employee> employees;
 	private ArrayList<PaySlip> paySlips;
+	private List<TaxBracket> taxBracketsFromJson;
 	private List<TaxBracket> taxBrackets;
-	private TestServiceLayer testServiceLayer;
+	//private TestServiceLayer testServiceLayer;
+	private TestServiceLayerNew testServiceLayerNew;
 	
 	public TestController( ArrayList<Employee> employees ) {
 		this.employees = employees;
+		this.taxBrackets = new ArrayList<TaxBracket>();
 		this.paySlips = new ArrayList<PaySlip>();
 		this.readTaxBracketData();
 		this.generatePaySlips();
@@ -29,9 +32,13 @@ public class TestController {
 	
 	private void generatePaySlips() {
 		for ( Employee employee : this.employees ) {
-			this.testServiceLayer = new TestServiceLayer( employee, this.prevTax, this.taxBracket );
-			this.testServiceLayer.createPaySlip();
-			this.paySlips.add( this.testServiceLayer.getPaySlip() );
+			//this.testServiceLayer = new TestServiceLayer( employee, this.prevTax, this.taxBracket );
+			//this.testServiceLayer.createPaySlip();
+			//this.paySlips.add( this.testServiceLayer.getPaySlip() );
+			this.testServiceLayerNew = new TestServiceLayerNew( this.taxBrackets, employee );
+			this.testServiceLayerNew.createPaySlip();
+			this.paySlips.add( this.testServiceLayerNew.getPaySlip() );
+			
 		}
 	}
 	
@@ -40,17 +47,18 @@ public class TestController {
 			TypeReference<List<TaxBracket>> typeReference = new TypeReference<List<TaxBracket>>() {};
 			InputStream inputStream = TypeReference.class.getResourceAsStream( TAX_FILE );
 			
-			this.taxBrackets = new ObjectMapper().readValue( inputStream, typeReference );
+			this.taxBracketsFromJson = new ObjectMapper().readValue( inputStream, typeReference );
 			
-			assert ( this.taxBrackets != null && !this.taxBrackets.isEmpty() ) : "Could not read tax brackets";
+			assert ( this.taxBracketsFromJson != null && !this.taxBracketsFromJson.isEmpty() ) : "Could not read tax brackets";
 			
-			for ( TaxBracket taxBracket : this.taxBrackets ) {
-				this.prevTax.put( taxBracket.getIncome() , taxBracket.getPreviousTax() );
-				this.taxBracket.put( taxBracket.getIncome() , taxBracket.getTaxRate() );
+			for ( TaxBracket taxBracket : this.taxBracketsFromJson ) {
+				this.taxBrackets.add( 0, taxBracket );
+				this.prevTax.put( taxBracket.getTaxThreshold() , taxBracket.getPreviousTax() );
+				this.taxBracket.put( taxBracket.getTaxThreshold() , taxBracket.getTaxRate() );
 			}
 			
 		} catch (Exception ex) {
-			System.out.println( "Unable to read data" );
+			System.out.println( "Unable to read data" + ex.getStackTrace() + ex.getMessage() + ex.getCause() );
 		}
 	}
 	
---
+++ test/src/main/java/com/seismatest/test/TestServiceLayerNew.java
@@ -0,0 +1,60 @@
+package com.seismatest.test;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TestServiceLayerNew {
+	
+	private List<TaxBracket> taxBrackets;
+	
+	private Employee employee;
+	private PaySlip paySlip;
+	
+	private TaxBracket taxBracket;
+	private long incomeTax;
+	
+	
+	public TestServiceLayerNew(List<TaxBracket> taxBrackets, Employee employee) {
+		super();
+		this.taxBrackets = taxBrackets;
+		this.employee = employee;
+		this.paySlip = new PaySlip( employee );
+	}
+	
+	public void determineTaxBracket() {
+		if ( this.employee.getAnnualSalary() > 0 ) {
+			Optional<TaxBracket> taxBracket = this.taxBrackets.stream()
+												.map( t -> t.getTaxBracketIfIncomeMatchesThreshold( this.employee.getAnnualSalary() ) )
+												.filter( t -> t != null )
+												.findFirst();
+			this.taxBracket = taxBracket.get();
+		} else {
+			this.taxBracket = new TaxBracket( 0, 0, 0 );
+		}	
+	}
+	
+	private void calculateTax() {
+		this.incomeTax = Math.round( ( this.taxBracket.getPreviousTax() + ( this.employee.getAnnualSalary() - this.taxBracket.getTaxThreshold() ) * this.taxBracket.getTaxRate() ) / 12.0 );
+	}
+	
+	public void createPaySlip() {
+		this.determineTaxBracket();
+		this.calculateTax();
+		
+		this.paySlip.setName( this.employee.getFirstName() + " " + this.employee.getLastName() );
+		
+		this.paySlip.setIncomeTax( (int) this.incomeTax );
+		
+		this.paySlip.setGrossIncome(  (int) Math.round( this.employee.getAnnualSalary() / 12.0 ) );
+		
+		this.paySlip.setNetIncome( (int) ( this.paySlip.getGrossIncome() - this.paySlip.getIncomeTax() ) );
+		
+		int empSuper =  Math.round( this.paySlip.getGrossIncome() * this.employee.getSuperRate() );
+		this.paySlip.setEmpSuper( empSuper > 0 ? empSuper : 0 );
+	}
+	
+	public PaySlip getPaySlip() {
+		return paySlip;
+	}
+
+}
